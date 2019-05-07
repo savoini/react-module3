a8ee2cccfcf0bea32c7add80b1645691e59b6f78
Gilberto <savoini@dac246.dac.unicamp.br>
2019-05-07 13:06:25 -0300

    Adicionando e Removendo todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,26 +1,45 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as TodoActions from './store/actions/todos';
 
-const TodoList = ({ todos }) => (
+const TodoList = ({ todos, addTodo, removeTodo }) => (
+  <Fragment>
     <ul>
-        {todos.map(todo => (
-            <li key={todo.id}>{todo.text}</li>
-        ))}
+      {todos.map(todo => (
+        <li key={todo.id}>
+          {todo.text}{' '}
+          <button type="submit" onClick={() => removeTodo(todo.id)}>
+            Remover
+          </button>
+        </li>
+      ))}
     </ul>
+    <button type="submit" onClick={() => addTodo('Fazer café')}>
+      Adicionar
+    </button>
+  </Fragment>
 );
 
 TodoList.propTypes = {
-    todos: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            text: PropTypes.string.isRequired
-        })
-    ).isRequired
+  addTodo: PropTypes.func.isRequired,
+  removeTodo: PropTypes.func.isRequired,
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
 };
 
 const mapStateToProps = state => ({
-    todos: state.todos
+  todos: state.todos,
 });
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = dispatch => bindActionCreators(TodoActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(TodoList);
