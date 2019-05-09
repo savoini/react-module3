9198a5f40eceb071266fe4a49f4d13bb6405ade5
Gilberto <savoini@dac246.dac.unicamp.br>
2019-05-09 08:07:43 -0300

    Repo Duplicado

--- src/store/sagas/favorites.js
+++ src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 
 import api from '../../services/api';
 import { Creators as FavoritesActions } from '../ducks/favorites';
@@ -7,14 +7,22 @@ export function* addFavorite(action) {
   try {
     const { data } = yield call(api.get, `/repos/${action.payload.repository}`);
 
-    const repositoryData = {
-      id: data.id,
-      name: data.full_name,
-      description: data.description,
-      url: data.html_url,
-    };
+    const isDuplicate = yield select(state => state.favorites.data.find(favorite => favorite.id === data.id));
 
-    yield put(FavoritesActions.addFavoriteSuccess(repositoryData));
+    if (isDuplicate) {
+      yield put(
+        FavoritesActions.addFavoriteFailure('Erro ao buscar repositório: Repositório duplicado'),
+      );
+    } else {
+      const repositoryData = {
+        id: data.id,
+        name: data.full_name,
+        description: data.description,
+        url: data.html_url,
+      };
+
+      yield put(FavoritesActions.addFavoriteSuccess(repositoryData));
+    }
   } catch (error) {
     console.log(error);
     yield put(
